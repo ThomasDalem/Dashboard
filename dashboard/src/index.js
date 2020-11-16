6f0bb4f55184dff0a55d770c36209ecebe716116
Marini Tony <Marini@desktop-o4f085k.home>
2020-11-16 16:59:27 +0100

    [ADD] sass and router module)

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import './styles/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
